536a47a3ffffa03d3df78dd4488e297467443603
Toy <xyz307322186@163.com>
2017-10-03 22:04:11 +0800

    17-9-3

---
+++ first_python.py
@@ -0,0 +1,5 @@
+#!/user/bin/env python3
+# -*- coding:utf-8 -*-
+
+print('100+200=',100+200)
+print('I love my motherland!')
---
+++ square.py
@@ -0,0 +1,5 @@
+#!/user/bin/env python3
+# -*- coding:utf-8 -*-
+
+l = float(input('请输入正方形的边长：'))
+print('边长为',l,'的正方形的面积为：',l*l)
